21f50afe7d57a72c17cb48b9c813cd3202e67a70
mrtn.maturano@gmail.com <mrtn.maturano@gmail.com>
2022-07-16 19:01:01 -0300

    first commit

---
+++ src/test/java/com/maturanomartin/technicaltest/TechnicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maturanomartin.technicaltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
