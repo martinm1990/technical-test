5349aadef0196dc09bd75829d2222b70bd7a8242
mrtn.maturano@gmail.com <mrtn.maturano@gmail.com>
2022-07-16 19:29:42 -0300

    add configuration, database scripts and flyway

---
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -0,0 +1,2 @@
+insert into super_hero (name,age,skill) values ('Superman',1000, 'can fly, superhuman strength');
+insert into super_hero (name,age,skill) values ('Spiderman',17, 'spider sense, Wallcrawling');
